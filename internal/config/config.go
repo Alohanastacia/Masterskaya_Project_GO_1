ac2bf5cf61a328f0557110912a6f118a888c8e66
Olga <s.miroslavna@yandex.ru>
2024-12-17 13:34:08 +0300

    [RS-6]Create ENV file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,27 @@
 package config
 
 import (
-	"log"
-	"os"
-
+	"database/sql"
+	"fmt"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 type Config struct {
 	Env string `yaml:"env" env-default:"prod"`
+}
+
+type ENVConfig struct {
+	dbHost     string `env:"DB_HOST"`
+	dbPort     int    `env:"DB_PORT"`
+	dbUser     string `env:"DB_USER"`
+	dbPassword string `env:"DB_PASSWORD"`
+	dbDbname   string `env:"DB_DBNAME"`
+	appPort    int    `env:"APP_PORT"`
+	appEnv     string `env:"APP_ENV"`
 }
 
 func NewConfig() Config {
@@ -32,4 +44,64 @@ func NewConfig() Config {
 		log.Fatalf("cannot read config: %s", &err)
 	}
 	return cfg
+}
+
+func StorageConfig() (ENVConfig, error) {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		log.Fatal("DB_HOST is not set")
+	}
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		log.Fatal("DB_PORT is not set")
+	}
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		log.Fatal("DB_NAME is not set")
+
+	}
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		log.Fatal("DB_USER is not set")
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatal("DB_PASSWORD is not set")
+	}
+	appPort := os.Getenv("APP_PORT")
+	if appPort == "" {
+		log.Fatal("APP_PORT is not set")
+	}
+	appEnv := os.Getenv("APP_ENV")
+	if appEnv == "" {
+		log.Fatal("APP_ENV is not set")
+	}
+	return ENVConfig{
+		dbHost:     dbHost,
+		dbPort:     dbPort,
+		dbUser:     dbUser,
+		dbPassword: dbPassword,
+		dbDbname:   dbName,
+		appPort:    appPort,
+		appEnv:     appEnv,
+	}, nil
+}
+
+func ConnectFile(*sql.DB, error) {
+	connStr := fmt.Sprintf(
+		`host=%s port=%s name=%s user=%s password=%s app_port=%s app_env=%s`,
+		dbHost, dbPort, dbName, dbUser, dbPassword, appPort, appEnv)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return db, nil
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
 }
